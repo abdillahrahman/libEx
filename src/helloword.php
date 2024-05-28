294ce4baa3aa379fca17e7ed339e47d501c84e96
Abdillah Rahman <abdillahrahman2912@gmail.com>
2024-05-28 09:27:45 +0700

    first commut

---
+++ src/helloword.php
@@ -0,0 +1,10 @@
+<?php
+namespace abdillahrahman/libhello;
+
+class helloworld{
+    public function sayhello($nama){
+        return "hello $nama selamat datang di libraray sederhana";
+    }
+}
+
+?>
